cc40ffd751cfa48b8fa47b7a7d74814dc62c8e35
Alonso <alonsolopezdominguez3@gmail.com>
2025-01-14 14:33:43 -0600

    Banner 'Sobre la app' actualizado

--- app/src/main/java/com/example/proyecto_cuentas_claras/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_cuentas_claras/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.proyecto_cuentas_claras
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -49,14 +51,37 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                // Aquí puedes mostrar un diálogo o navegar a una nueva actividad
+                mostrarInformacionApp()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun mostrarInformacionApp() {
+        val creditos =
+            "Desarrolladores:\n\nSalvador Trejo Martínez\nAlonso Domínguez López\nGuillermo Ordaz Rodríguez\nRaúl Cardoso Acevedo"
+        AlertDialog.Builder(this)
+            .setTitle("Cuentas claras")
+            .setMessage(creditos + "\n\nTodos los derechos reservados.\n\nVersión 1.0")
+            .setPositiveButton("Aceptar") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
